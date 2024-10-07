20900fd3c7c539fb657c373243bcac8ff294beb4
ROHIT BADGUJAR <badgujarrohit7@gmail.com>
2024-10-07 15:30:48 -0500

    day1pactice

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            Console.WriteLine("Hello World!");
+
+            string name = "John";
+            string dept = "Sales";
+
+            byte a = 111;
+            byte b = 111;
+            byte c;
+
+            try
+            {
+                checked
+                {
+                    c = (byte)((byte)a * (byte)b);
+                }
+                Console.WriteLine("The result of the multiplication is: " + c);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow occurred during byte multiplication. The result is too large to fit in a byte.");
+            }
+        }
+    }
+}
