92fecd04c50978c768ed70c7a48589972508e11b
ROHIT BADGUJAR <badgujarrohit7@gmail.com>
2024-10-07 17:18:10 -0500

    implimented thread pool

---
+++ threadPool_Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class ThreadPoolDemo
+    {
+        static void Main(string[] args)
+        {
+            // Set the minimum and maximum threads for the thread pool once, outside the loop
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(4, 4);
+
+            // Queue 200 work items to the thread pool
+            for (int i = 0; i < 200; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(MyMethod));
+            }
+
+            Console.ReadLine();
+        }
+
+        // Method to be executed by thread pool threads
+        public static void MyMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}, " +
+                             $"Thread ID: {thread.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+    }
+}
